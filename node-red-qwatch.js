e0ee56518d15f16bdf2c70e2da6f32163fa6a105
Tanaka Seigo <seigo@1ft-seabass.jp>
2021-01-27 22:06:55 +0900

    v0.0.1

---
+++ node-red-qwatch.js
@@ -0,0 +1,47 @@
+const DigestFetch = require('digest-fetch');
+
+module.exports = function (RED) {
+
+  function QwatchConfig(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+
+    this.serveraddress = config.serveraddress;
+    this.serverport = config.serverport;
+  }
+  RED.nodes.registerType("qwatch-config", QwatchConfig, {
+    credentials: {
+      loginid: {type:"text"},
+      loginpassword: {type:"password"}
+    }
+  });
+
+  function QwatchPhotoCapture(config) {
+    RED.nodes.createNode(this, config);
+
+    this.current_setting = RED.nodes.getNode(config.setting);
+
+    console.log('this.current_setting',this.current_setting);
+    
+    let username = this.current_setting.credentials.loginid;
+    let password = this.current_setting.credentials.loginpassword;
+    let IPAddress = this.current_setting.serveraddress;
+    let httpPort = this.current_setting.serverport;
+    var node = this;
+    node.on('input', async function (msg) {
+
+      const client = new DigestFetch(username , password, { algorithm: 'MD5' });
+      const url = 'http://' + IPAddress + ':' + httpPort + '/snapshot.jpg';
+      const options = {};
+      
+      this.status({ fill: "blue", shape: "ring", text: "[" + this.current_setting.name + "] connecting..." });
+      let resp = await client.fetch(url, options);
+      this.status({ fill: "green", shape: "dot", text: "[" + this.current_setting.name + "] get buffer" });
+
+      let buffer = await resp.buffer();
+      msg.payload = buffer;
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("qwatch-photo-capture", QwatchPhotoCapture, {});
+}
